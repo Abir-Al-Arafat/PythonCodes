4ccd4f77d6bcb68d9db5f79b5130f00dc300e276
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2020-07-01 22:20:07 +0600

    Create Multi.py

---
+++ Multi.py
@@ -0,0 +1,5 @@
+#input hours and work per hours and Calculate Payment
+hours = input("Input Hours: ") #Taking Input
+workHour = input("Input Work Per Hours: ") #Taking Input
+payment = float(hours) * float(workHour) #Multiplying
+print ("Salary:",payment,"$"); #Printing
